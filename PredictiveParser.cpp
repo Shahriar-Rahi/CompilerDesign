588467d94d551e142e86ef93d7497dac6e6e7fc2
Shahriar Rahi <md.alshahriarrahi@gmail.com>
2018-06-23 20:47:50 +0600

    Update PredictiveParser.cpp

--- PredictiveParser.cpp
+++ PredictiveParser.cpp
@@ -30,25 +30,25 @@ void s(){
 		s();
 		s();
 	}
-	else if(lookahead = '0')
+	else if(lookahead == '0')
 		match('0');
-	else if(lookahead = '1')
+	else if(lookahead == '1')
 		match('1');
-	else if(lookahead = '2')
+	else if(lookahead == '2')
 		match('2');
-	else if(lookahead = '3')
+	else if(lookahead == '3')
 		match('3');
-	else if(lookahead = '4')
+	else if(lookahead == '4')
 		match('4');
-	else if(lookahead = '5')
+	else if(lookahead == '5')
 		match('5');
-	else if(lookahead = '6')
+	else if(lookahead == '6')
 		match('6');
-	else if(lookahead = '7')
+	else if(lookahead == '7')
 		match('7');
-	else if(lookahead = '8')
+	else if(lookahead == '8')
 		match('8');
-	else if(lookahead = '9')
+	else if(lookahead == '9')
 		match('9');
 	//else if(isdigit(lookahead))
 		//match(lookahead);
